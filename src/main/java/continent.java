f1b020a468763779d99337d93e6e4e55004466b7
DerrickEllis <derrickellis2911@gmail.com>
2018-09-24 10:30:30 -0500

    Update continent.java
    updated to a list of territories instead of a list of strings

--- src/main/java/continent.java
+++ src/main/java/continent.java
@@ -5,15 +5,15 @@ import java.util.Objects;
 public class continent {
 
 	String name;
-	List<String> has;
-	List<String> terrs;
+	List<territory> has; //TERRITORIES THAT MAKE UP THE CONTINENT
+	int contVal;
 	
 	
 	//CONSTRUCTORS
 	public continent(String name) {
 		this.name = name;
 	}
-	public continent(String name, List<String> has) {
+	public continent(String name, List<territory> has) {
 		this.name = name;
 		this.has = has;
 	}
@@ -24,27 +24,39 @@ public class continent {
 	 * ADDS A LIST OF TERRITORIES TO THE CONTINENT
 	 * 
 	 */ 
-	public void setTerritories(List<String> list) {
+	public void setTerritories(List<territory> list) {
 		this.has = list;
+	}
+	
+	/**
+	 * ADDS VALUE TO THE CONTINENT
+	 * 
+	 */ 
+	public void setContinentValue(int val) {
+		this.contVal = val;
+	}
+	public int getContinentValue() {
+		return this.contVal;
+	}
+	
+	/**
+	 * PRINTS THE LIST OF TERRITORIES OF THE CONTINENT
+	 * 
+	 */
+	public void printContinentTerritories(){
+		System.out.println("");
+		System.out.println(name + " includes the following territories: ");
+		for(int i = 0; i < has.size(); i++) {
+			System.out.println(has.get(i).name);
+		}
 	}
 	
 	/**
 	 * GETS THE LIST OF TERRITORIES OF THE CONTINENT
 	 * 
 	 */
-	public void getTerritories(){
-		System.out.println("");
-		System.out.println(name + " includes the following territories:");
-		for(int i = 0; i < has.size(); i++) {
-			System.out.println(has.get(i));
-		}
+	public List<territory> getTerritories(){
+		return this.has;
 	}
-	/*public String getOwner(){
-		//String owner = has.get(0);
-
-		for(int n = 1; n < has.size(); n++){
-
-		}
-	}*/
 	
 }
