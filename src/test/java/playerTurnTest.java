0c9b8b904ec7b3bdc5197941c690e8163428f96a
DerrickEllis <derrickellis2911@gmail.com>
2018-09-24 10:38:53 -0500

    Update playerTurnTest.java
    commented

--- src/test/java/playerTurnTest.java
+++ src/test/java/playerTurnTest.java
@@ -12,15 +12,15 @@ import java.io.InputStreamReader;
 public class playerTurnTest {
     @Test
     public void testingInput(){
-        ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
-        System.setIn(in);
-        player player = new player();
-        territory[] tList = new territory[] {new territory()};
-        List<player> playerList = new ArrayList<player>();
-        playerTurn playerTurn = new playerTurn();
-        playerTurn.chooseOption(player, tList, playerList);
-        in = new ByteArrayInputStream("3".getBytes());
-        System.setIn(in);
-        playerTurn.chooseOption(player, tList, playerList);
+//         ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+//         System.setIn(in);
+//         player player = new player();
+//         territory[] tList = new territory[] {new territory()};
+//         List<player> playerList = new ArrayList<player>();
+//         playerTurn playerTurn = new playerTurn();
+//         playerTurn.chooseOption(player, tList, playerList);
+//         in = new ByteArrayInputStream("3".getBytes());
+//         System.setIn(in);
+//         playerTurn.chooseOption(player, tList, playerList);
     }
 }
