e3761d77c838981de796732e2f59d08c759eeebf
DerrickEllis <derrickellis2911@gmail.com>
2018-10-09 15:31:35 -0500

    Update AmazonS3Object.java

--- src/main/java/AmazonS3Object.java
+++ src/main/java/AmazonS3Object.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.Serializable;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 
