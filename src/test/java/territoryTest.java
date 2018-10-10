e7d0549327f79be78a0e772c6137dc44c3b168ad
DerrickEllis <derrickellis2911@gmail.com>
2018-10-09 19:03:55 -0500

    Create territoryTest.java

---
+++ src/test/java/territoryTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class territoryTest {
+
+@Test
+public territoryTest() {
+//    String name, int player, int armies, List<territory> terrs
+//		this.name = name;
+//		this.numofArmiesHere = armies;
+//		this.isOwnedBy = player;
+//		this.adj_territories = terrs;
+	}
+  
+}
