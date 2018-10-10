f99fbf48bab7dd1592b090f5fd7bb197abcd1bfd
DerrickEllis <derrickellis2911@gmail.com>
2018-10-10 14:10:24 -0500

    Update playerTurnTest.java

--- src/test/java/playerTurnTest.java
+++ src/test/java/playerTurnTest.java
@@ -1,26 +1,25 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
+//import org.junit.Test;
 
 public class playerTurnTest {
-    @Test
-    public void testingInput(){
-//         ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
-//         System.setIn(in);
-//         player player = new player();
-//         territory[] tList = new territory[] {new territory()};
-//         List<player> playerList = new ArrayList<player>();
-//         playerTurn playerTurn = new playerTurn();
-//         playerTurn.chooseOption(player, tList, playerList);
-//         in = new ByteArrayInputStream("3".getBytes());
-//         System.setIn(in);
-//         playerTurn.chooseOption(player, tList, playerList);
-    }
+
+	@Test
+	public void playerTurnTest1() {
+		playerTurn testTurn = new playerTurn();
+		assertEquals(testTurn.valid,true);
+	}
+
+	@Test
+	public void playerTurnTest2() {
+		player p = new player();
+		board b = new board();
+		playerTurn testTurn = new playerTurn(p,b);
+		assertEquals(testTurn.player,p);
+		assertEquals(testTurn.board,b);
+	}
+
 }
