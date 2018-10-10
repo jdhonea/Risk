ec72acc18f0f35500895f94116d5e4fb48e0dfd9
DerrickEllis <derrickellis2911@gmail.com>
2018-10-09 19:14:43 -0500

    Update territoryTest.java

--- src/test/java/territoryTest.java
+++ src/test/java/territoryTest.java
@@ -1,15 +1,20 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
 public class territoryTest {
 
-@Test
-public territoryTest() {
-//    String name, int player, int armies, List<territory> terrs
-//		this.name = name;
-//		this.numofArmiesHere = armies;
-//		this.isOwnedBy = player;
-//		this.adj_territories = terrs;
+	@Test
+	public void territoryTest() {
+		    String name = "name";
+		    int player = 1;
+		    int armies = 2;
+		    List<territory> terrs = new ArrayList<territory>();
+		    territory testTerritory = new territory(name,player,armies,terrs);
+		    assertEquals(testTerritory.name,name);
+		    assertEquals(testTerritory.isOwnedBy,player);
+		    assertEquals(testTerritory.numofArmiesHere,armies);
+		    assertEquals(testTerritory.adj_territories,terrs);
 	}
-  
 }
