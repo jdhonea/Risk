865b61ee8024cc8cf819b2f4c681e02044a89cd6
DerrickEllis <derrickellis2911@gmail.com>
2018-10-09 19:37:19 -0500

    Update territoryTest.java

--- src/test/java/territoryTest.java
+++ src/test/java/territoryTest.java
@@ -1,14 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-//import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
-//import org.junit.Test;
 
 public class territoryTest {
 
 	@Test
-	public void territoryTest() {
+	public void territoryTest1() {
 		    String name = "name";
 		    int player = 1;
 		    int armies = 2;
@@ -18,5 +16,27 @@ public class territoryTest {
 		    assertEquals(testTerritory.isOwnedBy,player);
 		    assertEquals(testTerritory.numofArmiesHere,armies);
 		    assertEquals(testTerritory.adj_territories,terrs);
+	}
+	
+	@Test
+	public void territoryTest2() {
+		String name = "name";
+		int player = 1;
+		int armies = 2;
+		territory testTerritory = new territory(name,player,armies);
+		assertEquals(testTerritory.name,name);
+		assertEquals(testTerritory.isOwnedBy,player);
+		assertEquals(testTerritory.numofArmiesHere,armies);
+	}
+
+	@Test
+	public void territoryTest3() {
+		String name = "name";
+		int territoryNumber = 10;
+		List<territory> terrs = new ArrayList<territory>();
+		territory testTerritory = new territory(name,territoryNumber,terrs);
+		assertEquals(testTerritory.name,name);
+		assertEquals(testTerritory.territoryNumber,territoryNumber);
+		assertEquals(testTerritory.adj_territories,terrs);
 	}
 }
