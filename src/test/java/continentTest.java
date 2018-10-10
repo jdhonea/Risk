31bd2ffa9bd9fa0c56c7bcd91ccf28c7eb4a5844
DerrickEllis <derrickellis2911@gmail.com>
2018-10-10 16:54:12 -0500

    Update continentTest.java

--- src/test/java/continentTest.java
+++ src/test/java/continentTest.java
@@ -23,4 +23,37 @@ public class continentTest {
 		assertEquals(contTest.has,has);
 	}
 	
+	@Test
+	public void setTerritoriesTest() {
+		String name = "testContinent";
+		List<territory> has = new ArrayList<territory>();
+		continent contTest = new continent(name);
+		contTest.setTerritories(has);
+		assertEquals(contTest.has,has);
+	}
+	
+	@Test
+	public void getTerritoriesTest() {
+		String name = "testContinent";
+		List<territory> has = new ArrayList<territory>();
+		continent contTest = new continent(name);
+		contTest.setTerritories(has);
+		assertEquals(contTest.getTerritories(),has);
+	}
+	
+	@Test
+	public void setContinentValueTest() {
+		String name = "testContinent";
+		continent contTest = new continent(name);
+		contTest.setContinentValue(7);
+		assertEquals(contTest.contVal,7);
+	}
+	
+	@Test
+	public void getContinentValueTest() {
+		String name = "testContinent";
+		continent contTest = new continent(name);
+		contTest.setContinentValue(5);
+		assertEquals(contTest.getContinentValue(),5);
+	}
 }
