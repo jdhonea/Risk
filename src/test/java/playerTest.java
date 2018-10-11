752ec1e341bd93349a379531e69b4647d1f6ae8c
DerrickEllis <derrickellis2911@gmail.com>
2018-10-11 15:18:28 -0500

    Update playerTest.java

--- src/test/java/playerTest.java
+++ src/test/java/playerTest.java
@@ -203,4 +203,173 @@ public class playerTest {
 		testPlayer.resetCardsContainedOwnedTerritory();
 		assertEquals(testPlayer.cardsContainedOwnedTerritory,false);
 	}
+	
+	@Test
+	public void canTradeTest() {
+		player testPlayer = new player();
+		testPlayer.canTrade = true;
+		assertEquals(testPlayer.canTrade(),true);
+	}
+
+	@Test
+	public void setWinnerTrueTest() {
+		player testPlayer = new player();
+		testPlayer.setWinner(true);
+		assertEquals(testPlayer.wonWholeGame,true);
+	}
+	@Test
+	public void setWinnerFalseTest() {
+		player testPlayer = new player();
+		testPlayer.setWinner(false);
+		assertEquals(testPlayer.wonWholeGame,false);
+	}
+
+	@Test
+	public void getWinnerTest() {
+		player testPlayer = new player();
+		testPlayer.setWinner(false);
+		assertEquals(testPlayer.getWinner(),false);
+	}
+
+	@Test
+	public void setwinnerofbattleTrueTest() {
+		player testPlayer = new player();
+		testPlayer.setwinnerofbattle(true);
+		assertEquals(testPlayer.win,true);
+	}
+	
+	@Test
+	public void setwinnerofbattleFalseTest() {
+		player testPlayer = new player();
+		testPlayer.setwinnerofbattle(false);
+		assertEquals(testPlayer.win,false);
+	}
+
+	@Test
+	public void getwinnerofbattleTest() {
+		player testPlayer = new player();
+		testPlayer.setwinnerofbattle(false);
+		assertEquals(testPlayer.getwinnerofbattle(),false);
+	}
+
+	@Test
+	public void setloserofbattleTrueTest() {
+		player testPlayer = new player();
+		testPlayer.setloserofbattle(true);
+		assertEquals(testPlayer.lose,true);
+	}
+	
+	@Test
+	public void setloserofbattleFalseTest() {
+		player testPlayer = new player();
+		testPlayer.setloserofbattle(false);
+		assertEquals(testPlayer.lose,false);
+	}
+
+	@Test
+	public void getloserofbattleTest() {
+		player testPlayer = new player();
+		testPlayer.setloserofbattle(false);
+		assertEquals(testPlayer.getloserofbattle(),false);
+	}
+	
+	@Test
+	public void ownthisterritoryYesTest() {
+		territory testTerritory = new territory();
+		testTerritory.isOwnedBy = 1;
+		player testPlayer = new player();
+		testPlayer.playerNo = 1;
+		assertEquals(testPlayer.ownthisterritory(testTerritory),true);
+	}
+	
+	@Test
+	public void ownthisterritoryNoTest() {
+		territory testTerritory = new territory();
+		testTerritory.isOwnedBy = 1;
+		player testPlayer = new player();
+		testPlayer.playerNo = 3;
+		assertEquals(testPlayer.ownthisterritory(testTerritory),false);
+	}
+
+	@Test
+	public void canAttackTrueTest() {
+		territory testTerritory = new territory();
+		testTerritory.isOwnedBy = 1;
+		player testPlayer = new player();
+		testPlayer.playerNo = 3;
+		testPlayer.canAttack(testTerritory);
+		assertEquals(testPlayer.canAttack,true);
+	}
+	
+	@Test
+	public void canAttackFalseTest() {
+		territory testTerritory = new territory();
+		testTerritory.isOwnedBy = 3;
+		player testPlayer = new player();
+		testPlayer.playerNo = 3;
+		testPlayer.canAttack(testTerritory);
+		assertEquals(testPlayer.canAttack,false);
+	}
+
+	@Test
+	public void setAttackModeTest1() {
+		player testPlayer = new player();
+		testPlayer.setAttackMode(true);
+		assertEquals(testPlayer.isAttacking,true);
+		assertEquals(testPlayer.isDefending,false);
+	}
+	
+	@Test
+	public void setAttackModeTest2() {
+		player testPlayer = new player();
+		testPlayer.setAttackMode(false);
+		assertEquals(testPlayer.isAttacking,false);
+		assertEquals(testPlayer.isDefending,true);
+	}
+	
+	@Test
+	public void isAttackingTest1() {
+		player testPlayer = new player();
+		testPlayer.setAttackMode(false);
+		assertEquals(testPlayer.isAttacking(),false);
+	}
+	
+	@Test
+	public void isAttackingTest2() {
+		player testPlayer = new player();
+		testPlayer.setAttackMode(true);
+		assertEquals(testPlayer.isAttacking(),true);
+	}
+
+	
+	
+	@Test
+	public void setDefenseModeTest1() {
+		player testPlayer = new player();
+		testPlayer.setDefenseMode(true);
+		assertEquals(testPlayer.isAttacking,false);
+		assertEquals(testPlayer.isDefending,true);
+	}
+	
+	@Test
+	public void setDefenseModeTest2() {
+		player testPlayer = new player();
+		testPlayer.setDefenseMode(false);
+		assertEquals(testPlayer.isAttacking,true);
+		assertEquals(testPlayer.isDefending,false);
+	}
+	
+	@Test
+	public void isDefendingTest1() {
+		player testPlayer = new player();
+		testPlayer.setDefenseMode(false);
+		assertEquals(testPlayer.isDefending(),false);
+	}
+	
+	@Test
+	public void isDefendingTest2() {
+		player testPlayer = new player();
+		testPlayer.setDefenseMode(true);
+		assertEquals(testPlayer.isDefending(),true);
+	}
 }
