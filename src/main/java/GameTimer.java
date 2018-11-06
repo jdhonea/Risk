bc123f0170c231734170e80768101f871c59019f
DerrickEllis <derrickellis2911@gmail.com>
2018-11-06 17:47:04 -0600

    Create GameTimer.java
    class for time limit for each turn

---
+++ src/main/java/GameTimer.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GameTimer {
+	
+	public GameTimer() {		
+	}
+	
+    public String[] GameTimerTask(BufferedReader reader, String input) {
+    	String[] output = new String[2];
+    	reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int x = 13; // wait 5 seconds at most
+			long startTime = System.currentTimeMillis();
+			while ((System.currentTimeMillis() - startTime) < x * 1000
+			        && !reader.ready()) {
+			}
+			if (reader.ready()) {
+				input = reader.readLine();
+				output[0] = "-1";
+				output[1] = input;
+			    return output;
+			} else {
+				output[0] = "0";
+				output[1] = input;
+			    return output;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		output[0] = "0";
+		output[1] = input;
+		return output;
+	}
+}
