b10a5ad3e820dd938f53b7eb0f3c6af4c2fedc4a
DerrickEllis <derrickellis2911@gmail.com>
2018-11-08 12:08:27 -0600

    Create GameTimerTest.java

---
+++ src/test/java/GameTimerTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.BufferedReader;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+
+public class GameTimerTest {
+
+	@Test
+	public void GameTimer() {
+		BufferedReader reader = null;
+		String testStr = "";
+		GameTimer gTimer = new GameTimer();
+		String[] input = gTimer.GameTimerTask(reader, testStr);
+		assertEquals(input[0],"0");
+	}
+}	 
