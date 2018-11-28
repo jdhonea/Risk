8619427d8bd046c78cbbbeed6e25a4087d60107c
DerrickEllis <derrickellis2911@gmail.com>
2018-11-27 19:25:01 -0600

    Update GameTimerTest.java

--- src/test/java/GameTimerTest.java
+++ src/test/java/GameTimerTest.java
@@ -7,10 +7,13 @@ public class GameTimerTest {
 
 	@Test
 	public void GameTimer() {
-// 		BufferedReader reader = null;
-// 		String testStr = "";
-// 		GameTimer gTimer = new GameTimer();
-// 		String[] input = gTimer.GameTimerTask(reader, testStr);
-// 		assertEquals(input[0],"0");
+		BufferedReader reader = null;
+		String[] checker = {"0",""};
+		String testStr = "";
+		GameTimer gTimer = new GameTimer();
+		String[] input = gTimer.GameTimerTask(reader, testStr);
+		assertEquals(input[0],"0");
+		assertEquals(input[1],"");
 	}
-}	 
+	
+}	
