5a5f6be7b80c867afa59fbf014b79554a2fde8a0
DerrickEllis <derrickellis2911@gmail.com>
2018-12-01 15:50:28 -0600

    Update AttackProcessTest.java

--- src/test/java/AttackProcessTest.java
+++ src/test/java/AttackProcessTest.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.junit.jupiter.api.Test;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
 
 /** 
  * 
