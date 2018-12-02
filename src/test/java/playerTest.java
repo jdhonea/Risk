3886ba4ac27aa0d6c0a538bc441df9722015f23d
DerrickEllis <derrickellis2911@gmail.com>
2018-12-02 12:16:09 -0600

    Update playerTest.java

--- src/test/java/playerTest.java
+++ src/test/java/playerTest.java
@@ -3,16 +3,14 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
-
-import static org.junit.Assert.*;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+
+//import org.junit.Test;
 
 /** 
  * 
